3c4be3ee79d60fddc56add71a72050d39c5b3c19
AJ Dunn <andrewjamesdunn@gmail.com>
2023-12-03 00:45:48 +1030

    Update day01.js

--- aoc-2023/day01.js
+++ aoc-2023/day01.js
@@ -21,30 +21,30 @@ const linesPartOne = text.split('\n');
 const linesPartTwo = textPart2.split('\n');
 
 for (let line of linesPartTwo) {
-  // part 2 - first take care of edge cases for overlapping 
-	line = line.replace('twone', 21)
-	line = line.replace('eightwo', 82);
-  line = line.replace('eighthree', 83);
-	line = line.replace('oneight', 18);
-	line = line.replace('nineight', 98);
-  line = line.replace('fiveight', 58);
-  line = line.replace('threeight', 38);  
+        // part 2 - first take care of edge cases for overlapping
+        line = line.replace('twone', 21)
+        line = line.replace('eightwo', 82);
+        line = line.replace('eighthree', 83);
+        line = line.replace('oneight', 18);
+        line = line.replace('nineight', 98);
+        line = line.replace('fiveight', 58);
+        line = line.replace('threeight', 38);  
 
-	// part 2 - replace text numbers to 
-	line = line.replace(/one|two|three|four|five|six|seven|eight|nine/gi, function (x) {
-	return x
-    .replace('one', 1)
-    .replace('two', 2)
-    .replace('three', 3)
-    .replace('four', 4)
-    .replace('five', 5)
-    .replace('six', 6)
-    .replace('seven', 7)
-    .replace('eight', 8)
-    .replace('nine', 9);
+        // part 2 - replace text numbers to 
+        line = line.replace(/one|two|three|four|five|six|seven|eight|nine/gi, function (x) {
+        return x
+                .replace('one', 1)
+                .replace('two', 2)
+                .replace('three', 3)
+                .replace('four', 4)
+                .replace('five', 5)
+                .replace('six', 6)
+                .replace('seven', 7)
+                .replace('eight', 8)
+                .replace('nine', 9);
     }); 
  
-	// part 1
+  // part 1
   let numbersOnly = line.replace(/\D/g, '');
 
   if (numbersOnly.length === 1) {
